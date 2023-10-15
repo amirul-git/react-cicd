563dc2bd2cf2dd659c162a474629539e684d0865
amir-repository <achmadnassardhi@gmail.com>
2023-10-15 12:01:59 +0700

    Add proxy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    proxy: {
+      "/": "13.229.243.181:5173",
+    },
     watch: {
       usePolling: true,
     },
